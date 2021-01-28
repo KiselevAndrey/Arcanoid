56476f392ee5cde1fe2946f50f3c9033c585ef22
KiselevAndrey <andrey-199213@mail.ru>
2021-01-29 00:48:09 +0300

    v0.3.5

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    Vector3 _startPosition;
-    Rigidbody2D _rigidbody;
+    [SerializeField] PlayerMove playerPlatform;
+    [SerializeField, Min(1)] float startedSpeed;
+
+    Rigidbody2D _rb;
+    bool _isStarted;
 
     private void Start()
     {
-        _startPosition = transform.position;
-        _rigidbody = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     public void Reset()
     {
-        _rigidbody.velocity = Vector3.zero;
-        transform.position = _startPosition;
+        _isStarted = false;
+    }
+    
+    private void Update()
+    {
+        switch (_isStarted)
+        {
+            case true:
+
+                break;
+
+            case false:
+                PasteToPlayerPlatform();
+                break;
+        }
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    print(collision.gameObject.tag);
-    //    if (collision.gameObject.tag == BDNames.Wall)
-    //        print(collision.gameObject.tag);
-    //}
+    void PasteToPlayerPlatform()
+    {
+        transform.position = new Vector2(playerPlatform.transform.position.x, playerPlatform.transform.position.y + playerPlatform.transform.localScale.y);
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            GetRandomStartedForce();
+            _isStarted = true;
+        }
+    }
+
+    void GetRandomStartedForce()
+    {
+        Vector2 force = new Vector2(Random.Range(-100, 101), Random.Range(20, 101)).normalized;
+        _rb.AddForce(force * startedSpeed);
+    }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum MovingType { Keyboard, Mouse }
+
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField, Min(0)] int moveSpeed;
+    [SerializeField, Min(1)] int moveSpeed;
+    [SerializeField, Min(1)] float borderX;
+    [SerializeField] MovingType movingType;
 
+    Rigidbody2D _rigidbody;
     int _currentSpeed;
-
+    float _startPosY;
 
     // Start is called before the first frame update
     void Start()
     {
         _currentSpeed = moveSpeed;
+        _startPosY = transform.position.y;
+
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,27 +31,24 @@ public class PlayerMove : MonoBehaviour
 
     void MoveHorizontal()
     {
-        transform.position += new Vector3(Input.GetAxis(BDNames.Horizontal) * _currentSpeed * Time.deltaTime, 0);
-    }
+        switch (movingType)
+        {
+            case MovingType.Keyboard:
+                Vector2 moveInput = new Vector2(Input.GetAxisRaw(BDNames.Horizontal), 0);
+                _rigidbody.MovePosition(_rigidbody.position + moveInput.normalized * moveSpeed * Time.deltaTime);
+                break;
+            case MovingType.Mouse:
+                Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mouseWorldPos.y = _startPosY;
+                transform.position = mouseWorldPos;
+                break;
+            default:
+                break;
+        }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == BDNames.Ball)
-        {
-            print(collision.gameObject.tag);
-        }
-        if (collision.gameObject.tag == BDNames.Wall)
-        {
-            print(collision.gameObject.tag);
-            _currentSpeed = -moveSpeed;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == BDNames.Wall)
-        {
-            _currentSpeed = moveSpeed;
-        }
+        if (transform.position.x > borderX)
+            transform.position = new Vector2(borderX, transform.position.y);
+        else if(transform.position.x < -borderX)
+            transform.position = new Vector2(-borderX, transform.position.y);
     }
 }
